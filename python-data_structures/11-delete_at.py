b6eb54b26186933d37fb33ca26b378fa2e30974b
jeje-digifab <j.romand.digifab@gmail.com>
2024-05-14 20:52:58 +0200

    Fix code

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 
 
-def delete_at(my_list=[], idx=0):
-    del_list = my_list[:]
-    if idx < 0 or idx >= len(del_list):
-        return my_list
-    else:
-        del_list.pop(idx)
-        return del_list
+def delete_at(my_list, idx):
+    if idx < 0 or idx >= len(my_list):
+        return None
+
+    del my_list[idx]
+    return my_list
 
 
 if __name__ == "__main__":
