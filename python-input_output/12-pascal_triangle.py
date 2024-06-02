90f7033a7882ad192206e27f9de0b906843de862
jeje-digifab <j.romand.digifab@gmail.com>
2024-06-02 19:09:01 +0200

    Update file

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -3,12 +3,24 @@
 
 
 def pascal_triangle(n):
-    triangle = []
-    if n < 0:
-        return triangle = []
+    """
+    Returns a list of lists of integers representing Pascal's triangle
+    of size n.
+    Returns an empty list if n <= 0.
+    """
+    if n <= 0:
+        return []
 
-    if not isinstance(n, int):
-        return triangle = []
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
 
 
 if __name__ == "__main__":
