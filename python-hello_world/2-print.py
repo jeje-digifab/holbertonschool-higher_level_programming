8f08b7ba91178030581a63987a599e927938f8b4
jeje-digifab <j.romand.digifab@gmail.com>
2024-09-02 16:31:58 +0200

    Initial release

---
+++ python-hello_world/2-print.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+"""
+Python script that prints exactly "Programming is
+like building a multilingual puzzle.
+"""
+print('"Programming is like building a multilingual puzzle')
---
+++ python-hello_world/5-print_string.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Holberton School"
+print(f"{str}", end='')
+print(f"{str}")
+print(f"{str[:9]}")
