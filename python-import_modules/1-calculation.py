fe9617ab41a07e920deda555cca5866413dfb7ff
jeje-digifab <j.romand.digifab@gmail.com>
2024-09-06 03:07:52 +0200

    Fix comment

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 This module imports basic arithmetic functions (add, sub, mul, div)
-from 'calculator_1' and performs operations on two integers.
+from 'calculator_1.py' and performs operations on two integers.
 """
 
 from calculator_1 import add, sub, mul, div
@@ -11,7 +11,7 @@ b = 5
 
 
 if __name__ == "__main__":
-    print(f"{a} + {b} = {add(a, b)}")
-    print(f"{a} - {b} = {sub(a, b)}")
-    print(f"{a} * {b} = {mul(a, b)}")
-    print(f"{a} / {b} = {div(a, b)}")
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
