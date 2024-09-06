b2d0f071d6abf473f4086ad7753d0a40239e84fb
jeje-digifab <j.romand.digifab@gmail.com>
2024-09-06 03:18:26 +0200

    Delete comment

--- python-import_modules/5-variable_load.py
+++ python-import_modules/5-variable_load.py
@@ -1,19 +1,6 @@
 #!/usr/bin/python3
 
-
-"""
-This script imports the variable 'a' from the module 'variable_load_5.py'
-and prints its value.
-
-Requirements:
-1. The variable 'a' is imported from 'variable_load_5.py'.
-2. No usage of wildcard imports (e.g., `*`) or `__import__`.
-3. The code inside the `if __name__ == "__main__":` block ensures that
-the script does not execute
-   when the module is imported in another script, and only executes when
-   run directly.
-"""
+from variable_load_5 import a
 
 if __name__ == "__main__":
-    from variable_load_5 import a
     print("{}".format(a))
