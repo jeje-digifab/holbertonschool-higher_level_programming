d27776e6458689c74c6bda515f3873bb3989670c
jeje-digifab <j.romand.digifab@gmail.com>
2024-09-11 02:11:34 +0200

    Fix code task 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -7,12 +7,12 @@ def max_integer(my_list=[]):
     :param my_list: List of integers to evaluate (default is an empty list)
     :return: The maximum integer in the list, or None if the list is empty
     """
-    # Initialize the maximum value to 0
-    maximum = 0
-
     # Return None if the list is empty
     if len(my_list) == 0:
         return (None)
+
+    # Initialize the maximum value to 0
+    maximum = my_list[0]
 
     # Iterate through each number in the list
     for number in my_list:
