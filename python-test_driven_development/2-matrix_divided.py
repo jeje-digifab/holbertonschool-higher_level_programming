e18fcd12d05988f7b47b1691fcebaf8e0aa39bb3
jeje-digifab <j.romand.digifab@gmail.com>
2024-09-13 14:56:49 +0200

    Fix task 2

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,3 +1,9 @@
+#!/usr/bin/python3
+"""
+This script contains utility functions for basic arithmetic operations.
+"""
+
+
 def matrix_divided(matrix, div):
     """
     Divides all elements of a matrix by a given divisor and returns
@@ -22,29 +28,24 @@ def matrix_divided(matrix, div):
     ZeroDivisionError: If `div` is zero.
     """
 
-    # Check if matrix is a list of lists
     if not isinstance(matrix, list) or \
             not all(isinstance(row, list) for row in matrix):
         raise TypeError(
             "matrix must be a matrix (list of lists) of integers/floats")
 
-    # Check if all elements in matrix are integers or floats
     if not all(isinstance(el, (int, float)) for row in matrix for el in row):
         raise TypeError(
             "matrix must be a matrix (list of lists) of integers/floats")
 
-    # Check if all rows in matrix have the same size
     row_length = len(matrix[0])
     if not all(len(row) == row_length for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
 
-    # Check if div is a number and not zero
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    # Perform division and round results
     result = []
     for row in matrix:
         divided_row = [round(el / div, 2) for el in row]
