211f02d12e4370638a162b7a90eec004f85f7c71
jeje-digifab <j.romand.digifab@gmail.com>
2024-09-15 17:52:37 +0200

    Fix code task 11

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
-"""
-This script contains utility functions for basic arithmetic operations.
-"""
-
-
 def multiply_list_map(my_list=[], number=0):
-    new_list = my_list.copy()
-    new_list = list(map(lambda i: i * number, new_list))
-    return (new_list)
+    return (list(map(lambda i: i * number, my_list)))
