c3cd103339aaeb91d132b31f7d237cf3b85ef1a3
jeje-digifab <j.romand.digifab@gmail.com>
2024-09-16 10:31:58 +0200

    Add task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This script contains utility functions for basic arithmetic operations.
+"""
+
+
+class Square:
+    """A class representing a square.
+
+    This class provides methods to calculate the area and perimeter
+    of a square.
+    """
+
+    def __init__(self, size):
+        """Initialize a new square with a given size.
+
+        Args:
+            size (float): The length of a side of the square.
+        """
+        pass
