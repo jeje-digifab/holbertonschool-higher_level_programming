a052e169372a2e9a4a32052ab154cc174c2fe479
jeje-digifab <j.romand.digifab@gmail.com>
2024-10-30 15:18:40 +0100

    Fix task 1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,10 +1,4 @@
 -- Script that creates the MySQL server user user_0d_1.
-
--- Create the user if it does not exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
--- Grant all privileges to the user
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-
--- Flush privileges to ensure the changes take effect
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
