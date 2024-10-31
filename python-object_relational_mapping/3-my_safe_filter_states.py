a10f3f67e3f02fa0cff9c3e7e78eb28c35eb48d2
jeje-digifab <j.romand.digifab@gmail.com>
2024-10-31 15:23:32 +0100

    Add task 3

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+Script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument.
+"""
+
+import MySQLdb
+import sys
+
+
+def main():
+    """
+    Connects to a MySQL database and retrieves states with a given name.
+    """
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    try:
+        if len(sys.argv) < 5 or len(sys.argv) >= 6:
+            raise IndexError("Expected exactly 5 arguments")
+        filter = sys.argv[4]
+
+    except IndexError as e:
+        print(e)
+        sys.exit(1)
+
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name,
+        charset="utf8"
+    )
+    cur = conn.cursor()
+    # HERE I have to know SQL to grab all states in my database
+    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC",
+                (filter,))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
