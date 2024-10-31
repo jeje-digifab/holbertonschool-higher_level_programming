25c13a6a2fded328b61488df2e51e82365a6e6d5
jeje-digifab <j.romand.digifab@gmail.com>
2024-10-31 22:44:34 +0100

    Add docstring

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -4,14 +4,21 @@ Script that takes in the name of a state as an argument and lists
 all cities of that state, using the database hbtn_0e_4_usa
 """
 
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Integer, String
 
 
 Base = declarative_base()
 
 
 class State(Base):
+    """
+    State class representing the states table in the database.
+
+    Attributes:
+        id (int): The unique identifier for the state.
+        name (str): The name of the state.
+    """
     __tablename__ = 'states'
     id = Column("id", Integer, primary_key=True,
                 nullable=False, autoincrement=True)
